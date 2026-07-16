629489231b97e91feaaabfef64735bbb55924307
jiafan-tiaocan <jiafan.jf@qq.com>
2026-07-16 19:12:53 +0800

    Add published articles to sidebar

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,10 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+ExternalPlugin.RecentNotes({
+  filter: (page: { slug?: string; frontmatter?: { publish?: boolean } }) =>
+    page.slug !== "404" && page.frontmatter?.publish === true,
+})
 
 const config = await loadQuartzConfig()
 export default config
